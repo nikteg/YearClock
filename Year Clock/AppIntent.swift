6a38c3d5f7f7b2b5bfbd0fb8d201612d604e33e7
Niklas <me@sodapop.se>
2025-09-14 12:05:54 +0200

    Init

---
+++ Year Clock/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Year Clock
+//
+//  Created by Niklas on 2025-09-14.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ Year Clock/Year_ClockBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Year_ClockBundle.swift
+//  Year Clock
+//
+//  Created by Niklas on 2025-09-14.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Year_ClockBundle: WidgetBundle {
+    var body: some Widget {
+        Year_Clock()
+    }
+}
