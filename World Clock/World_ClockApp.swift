f970ea58027c1c8198464d6834e06350ade96791
Niklas <me@sodapop.se>
2025-09-14 12:01:01 +0200

    Initial Commit

---
+++ World Clock/World_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_ClockApp.swift
+//  World Clock
+//
+//  Created by Niklas on 2025-09-14.
+//
+
+import SwiftUI
+
+@main
+struct World_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
